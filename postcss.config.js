a332fbaffc692a4d8e30c839dc27c26fb54d3946
Georgios Kotziabassis <kotziabassis@gmail.com>
2020-09-04 03:22:00 +0200

    Set up PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,16 @@
+const purgecss = [
+  '@fullhuman/postcss-purgecss',
+  {
+    content: ['./components/**/*.js', './pages/**/*.js'],
+    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+  },
+]
+
 module.exports = {
-  plugins: ['tailwindcss', 'autoprefixer'],
+  plugins: [
+    'tailwindcss',
+    'autoprefixer',
+    'postcss-import',
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+  ],
 }
