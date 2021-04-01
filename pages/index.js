11dbeeb27b5445db96ae73856260ca859ab7dc48
Georgios Kotziabassis <kotziabassis@gmail.com>
2021-04-01 23:16:05 +0200

    ♻️ Update tech stack

--- pages/index.js
+++ pages/index.js
@@ -201,6 +201,9 @@ const Home = () => {
               JavaScript
             </div>
             <div className='px-4 py-2 mb-4 mr-4 leading-6 text-center border-2 rounded select-none border-dark-gray hover:border-pink-600 hover:text-pink-600 dark:border-white dark:hover:border-indigo-600 dark:hover:text-indigo-600'>
+              TypeScript
+            </div>
+            <div className='px-4 py-2 mb-4 mr-4 leading-6 text-center border-2 rounded select-none border-dark-gray hover:border-pink-600 hover:text-pink-600 dark:border-white dark:hover:border-indigo-600 dark:hover:text-indigo-600'>
               React
             </div>
             <div className='px-4 py-2 mb-4 mr-4 leading-6 text-center border-2 rounded select-none border-dark-gray hover:border-pink-600 hover:text-pink-600 dark:border-white dark:hover:border-indigo-600 dark:hover:text-indigo-600'>
@@ -210,13 +213,13 @@ const Home = () => {
               Next.js
             </div>
             <div className='px-4 py-2 mb-4 mr-4 leading-6 text-center border-2 rounded select-none border-dark-gray hover:border-pink-600 hover:text-pink-600 dark:border-white dark:hover:border-indigo-600 dark:hover:text-indigo-600'>
-              Gatsby
-            </div>
-            <div className='px-4 py-2 mb-4 mr-4 leading-6 text-center border-2 rounded select-none border-dark-gray hover:border-pink-600 hover:text-pink-600 dark:border-white dark:hover:border-indigo-600 dark:hover:text-indigo-600'>
               Tailwind CSS
             </div>
             <div className='px-4 py-2 mb-4 mr-4 leading-6 text-center border-2 rounded select-none border-dark-gray hover:border-pink-600 hover:text-pink-600 dark:border-white dark:hover:border-indigo-600 dark:hover:text-indigo-600'>
               Node.js
+            </div>
+            <div className='px-4 py-2 mb-4 mr-4 leading-6 text-center border-2 rounded select-none border-dark-gray hover:border-pink-600 hover:text-pink-600 dark:border-white dark:hover:border-indigo-600 dark:hover:text-indigo-600'>
+              PostgreSQL
             </div>
             <div className='px-4 py-2 mb-4 mr-4 leading-6 text-center border-2 rounded select-none border-dark-gray hover:border-pink-600 hover:text-pink-600 dark:border-white dark:hover:border-indigo-600 dark:hover:text-indigo-600'>
               MongoDB
