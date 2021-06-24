168ae313b91e1c95c646df72358771d1dea16725
Georgios Kotziabassis <kotziabassis@gmail.com>
2021-06-24 22:54:16 +0200

    ✨ Add GraphQL to tech stack

--- pages/index.js
+++ pages/index.js
@@ -213,10 +213,13 @@ const Home = () => {
               Next.js
             </div>
             <div className='px-4 py-2 mb-2 mr-2 leading-6 text-center border-2 rounded select-none border-dark-gray hover:border-pink-600 hover:text-pink-600 dark:border-white dark:hover:border-indigo-600 dark:hover:text-indigo-600'>
-              Tailwind CSS
+              Node.js
             </div>
             <div className='px-4 py-2 mb-2 mr-2 leading-6 text-center border-2 rounded select-none border-dark-gray hover:border-pink-600 hover:text-pink-600 dark:border-white dark:hover:border-indigo-600 dark:hover:text-indigo-600'>
-              Node.js
+              GraphQL
+            </div>
+            <div className='px-4 py-2 mb-2 mr-2 leading-6 text-center border-2 rounded select-none border-dark-gray hover:border-pink-600 hover:text-pink-600 dark:border-white dark:hover:border-indigo-600 dark:hover:text-indigo-600'>
+              Tailwind CSS
             </div>
             <div className='px-4 py-2 mb-2 mr-2 leading-6 text-center border-2 rounded select-none border-dark-gray hover:border-pink-600 hover:text-pink-600 dark:border-white dark:hover:border-indigo-600 dark:hover:text-indigo-600'>
               PostgreSQL
